cdad2e9beb912f0829a26288835793859df809ba
terrencetec <terrencetec@gmail.com>
2025-01-11 02:54:01 -0600

    test

--- tests/statistics/test_weibull.py
+++ tests/statistics/test_weibull.py
@@ -1,12 +1,12 @@
 """Test uclass.statistics.weibull"""
 import numpy as np
 
-import uclass
+import uclass.statistics.weibull
 
 
 k = 3.6
 lam = 5
-weibull = uclass.Weibull(lam=lam, k=k)
+weibull = uclass.statistics.weibull.Weibull(lam=lam, k=k)
 
 def test_mean():
     """Test mean"""
@@ -14,11 +14,13 @@ def test_mean():
     mean_true = 4.5055
     assert np.isclose(mean, mean_true)
 
+
 def test_mode():
     """Test mode"""
     mode = weibull.mode
     mode_true = 4.56785076
     assert np.isclose(mode, mode_true)
+
 
 def test_median():
     """Test median"""
@@ -26,11 +28,13 @@ def test_median():
     median_true = 4.5160095
     assert np.isclose(median, median_true)
 
+
 def test_variance():
     """Test variance"""
     variance = weibull.variance
     variance_true = 1.932382
     assert np.isclose(variance, variance_true)
+
 
 def test_std():
     """Test std"""
@@ -38,14 +42,28 @@ def test_std():
     std_true = 1.3901014
     assert np.isclose(std, std_true)
 
+
 def test_skewness():
     """Test skewness"""
     skewness = weibull.skewness
     skewness_true = 0.0005629389
     assert np.isclose(skewness, skewness_true)
 
+
 def test_kurtosis():
     """Test kurtosis"""
     kurtosis = weibull.kurtosis
     kurtosis_true = -0.2832548
     assert np.isclose(kurtosis, kurtosis_true)
+     
+
+def test_pdf():
+    """Test pdf"""
+    x = np.linspace(0, 10, 1024)
+    weibull.pdf(x)
+
+
+def test_pdf():
+    """Test pdf"""
+    x = np.linspace(0, 10, 1024)
+    weibull.cdf(x)
---
+++ uclass/database/mongo.py
@@ -0,0 +1,9 @@
+"""Mongo database class"""
+import pymongo.mongo_client
+import pandas
+
+
+class Mongo(pymongo.mongo_client.MongoClient):
+    """Mongo database class"""
+    def __init__(self, url):
+        pass
