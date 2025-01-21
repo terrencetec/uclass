4368ecf45c27b6c98bf2692216233da64d548157
terrencetec <terrencetec@gmail.com>
2025-01-20 19:40:21 -0600

    Test weibull5

--- tests/hhf_methods/test_weibull5.py
+++ tests/hhf_methods/test_weibull5.py
@@ -1,3 +1,4 @@
+"""Test Weibull5 method"""
 import numpy as np
 
 import uclass
@@ -5,7 +6,7 @@ import uclass
 
 def test_weibull5():
     """Test weibull5"""
-    hf = np.loadtxt("../data/co_23-01.txt")
+    hf = np.loadtxt("tests/data/co_23-01.txt")
     weibull5 = uclass.Weibull5(hf)
     hhf = weibull5.get_hhf(.95, .85)
     hhf_true = 10.5804297
